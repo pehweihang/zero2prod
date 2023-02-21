871062e9e45b886575881d354963ef52a9fb9354
weihang <pehweihang@live.com>
2023-02-21 06:58:42 +0100

    chapter 6

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -135,3 +135,32 @@ async fn subscribe_returns_400_when_data_is_missing() {
         );
     }
 }
+
+#[tokio::test]
+async fn subscribe_returns_400_whet_fields_are_present_but_invalid() {
+    let test_app = spawn_app().await;
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=notemail", "invalid email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", test_app.address))
+            .header("Client-Type", "x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "API did not fail with 400 with {}",
+            error_message
+        );
+    }
+}
