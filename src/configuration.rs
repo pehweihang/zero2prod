136f636fc74704032e9e3d48272ec035f1c54b22
weihang <pehweihang@live.com>
2023-02-21 03:12:19 +0100

    read env vars

--- src/configuration.rs
+++ src/configuration.rs
@@ -67,6 +67,8 @@ pub fn get_configuration() -> Result<Settings, config::ConfigError> {
         config::File::from(configuration_directory.join(environment.as_str())).required(true),
     )?;
 
+    settings.merge(config::Environment::with_prefix("app").separator("__"))?;
+
     settings.try_into()
 }
 
