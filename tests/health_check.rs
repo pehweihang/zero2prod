1e15f8a827645c7f9d013dac9e455a621474ab43
weihang <pehweihang@live.com>
2023-02-21 02:51:45 +0100

    use env for database

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,7 +1,6 @@
 use std::net::TcpListener;
 
 use once_cell::sync::Lazy;
-use secrecy::ExposeSecret;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use zero2prod::{
@@ -47,20 +46,16 @@ async fn spawn_app() -> TestApp {
 }
 
 async fn configure_database(database_settings: &DatabaseSettings) -> PgPool {
-    let mut connection = PgConnection::connect(
-        database_settings
-            .connection_string_without_db()
-            .expose_secret(),
-    )
-    .await
-    .expect("Failed to connect to Postgres");
+    let mut connection = PgConnection::connect_with(&database_settings.without_db())
+        .await
+        .expect("Failed to connect to Postgres");
 
     connection
         .execute(format!(r#"CREATE DATABASE "{}""#, database_settings.database_name).as_str())
         .await
         .expect("Failed to create database");
 
-    let connection_pool = PgPool::connect(database_settings.connection_string().expose_secret())
+    let connection_pool = PgPool::connect_with(database_settings.with_db())
         .await
         .expect("Failed to connect to Postgres");
 
